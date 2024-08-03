eb9f2da8facc77617c56cb5a0537219f2d18e36e
Chibuzor <115053675+Chiefpippi@users.noreply.github.com>
2024-08-02 23:39:39 -0400

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,11 +1,11 @@
 provider "aws"{
-region = "ap-south-1"
+region = "ca-central-1"
 }
 
 resource "aws_instance" "AWS_EC2INSTANCE"{
-ami = "ami-068e0f1a600cd311c"
+ami = "ami-07fb7b1409c393c50"
 instance_type = "t2.micro"
-key_name = "devopsmssjuly"
+key_name = "newbatch24"
 security_groups = ["launch-wizard-2"]
 tags = {
 Name = "terraformserver"
